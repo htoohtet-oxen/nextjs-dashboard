2566328527f2fe91818794fcea58d4d6da856f2e
htoohtet-oxen <htoohtet@oxen.com.sg>
2026-01-16 15:00:43 +0800

    auth, login, metadata,, and customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,20 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { Metadata } from "next";
+import Table from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <Table customers={customers}/>
+  );
 }
